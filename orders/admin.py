a120f19978e43d5b5cc1a3123636a8939f981d24
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-10-11 18:12:45 +0500

    Add orders models to admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from orders import models
 
-# Register your models here.
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    pass
